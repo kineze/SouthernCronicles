a31f67115ee9f69eb92410bd649f2db59460ee42
kineze <krishantharaka@outlook.com>
2025-07-26 16:40:41 +0530

    writer lab

--- resources/views/web/writingLab.blade.php
+++ resources/views/web/writingLab.blade.php
@@ -43,6 +43,27 @@
         </div>
     </div>
    
+
+
+
+
+    <div >
+        <h1 class="lg:text-4xl text-3xl mt-12 lg:mt-4 text-black font-extrabold tracking-widest uppercase text-center">Voices Unbound: Liberating the Next Wave of Asian Writers</h1>
+    
+        <div class="flex w-full justify-center flex-wrap py-12">
+            <div class="lg:w-4/12 w-full">
+                <img src="{{asset('/assets/img/kipenzi-art.webp')}}" alt="">
+            </div>
+
+            <div class="lg:w-6/12 flex flex-col items-center md:items-start justify-center w-full">
+                <img src="{{asset('assets/img/kipenzi-main-logo.webp')}}" class="w-80 p-3" alt="">
+                <p class="text-gray-700 text-justify py-3 p-3">Kipenzi reimagines storytelling from the ground up—breaking rules, shattering norms, and daring to explore what lies beyond the expected. Kipenzi’s work defies convention, stretching the very limits of narrative and reshaping how stories can be told. Kipenzi doesn’t just challenge the norms—it reinvents them, opening bold, uncharted paths in literature. This isn’t just innovation—it’s a literary revolution.</p>
+                <a href="https://kipenzi.me/all-ebooks" class="flex items-center gap-3 py-3 px-5 border w-fit border-main-blue/80 text-white rounded-full bg-black hover:text-white hover:shadow-lg">Kipenzi Connect</a>
+
+            </div>
+        </div>
+    
+    </div>
 </section>
 
 @endsection
