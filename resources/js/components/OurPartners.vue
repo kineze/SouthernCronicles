2952bdee1f64d756da67c8c83d4060f04825fcdb
dhanushkabndara <chathudhanushka77@gmail.com>
2025-07-26 11:30:58 +0530

    devlopment

--- resources/js/components/OurPartners.vue
+++ resources/js/components/OurPartners.vue
@@ -5,7 +5,7 @@
     </h1>
 
     
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4">
       <div
         v-for="partner in visiblePartners"
         :key="partner.id"
@@ -34,7 +34,7 @@ import axios from 'axios'
 const allPartners = ref([])
 const visiblePartners = ref([])
 const rowsToShow = ref(2)
-const cols = 6
+const cols = 7
 
 const fetchPartners = async () => {
   const { data } = await axios.get('/api/our-partners')
@@ -43,7 +43,7 @@ const fetchPartners = async () => {
 }
 
 const loadMore = () => {
-  rowsToShow.value += 5
+  rowsToShow.value += 7
   visiblePartners.value = allPartners.value.slice(0, rowsToShow.value * cols)
 }
 
