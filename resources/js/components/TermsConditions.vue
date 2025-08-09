bd86c5646da7a815fd3a0ebedbff87f16adff32a
dhanushkabndara <chathudhanushka77@gmail.com>
2025-08-09 13:10:16 +0530

    development

--- resources/js/components/TermsConditions.vue
+++ resources/js/components/TermsConditions.vue
@@ -69,10 +69,10 @@
 
       <!-- Footer -->
       <div class="mt-8 text-center text-base text-gray-900 dark:text-gray-300 leading-relaxed">
-        <p class="font-semibold text-lg">The Asian Literary Festival, Brussels</p>
+        <p class="font-semibold text-lg">The Asian Literary Festival, Gampaha</p>
         <p>Philippine Art & Culture Exchange</p>
-        <p>Chée de Roodebeek 148, 1200 Woluwe-Saint-Lambert</p>
-        <p>+32 496 06 81 86</p>
+        <p>The Asian Literary Agency, 16 Vishaka Road, Medagama, Gampaha</p>
+        <p>+94 77 556 4255</p>
         <p>info@theasianliteraryfestival.com</p>
       </div>
     </div>
