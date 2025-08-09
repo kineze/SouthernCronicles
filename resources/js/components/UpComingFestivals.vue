3e4bffd6303b6df661845f830831749c70c6aa87
kineze <krishantharaka@outlook.com>
2025-08-09 16:56:19 +0530

    update

--- resources/js/components/UpComingFestivals.vue
+++ resources/js/components/UpComingFestivals.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="max-w-screen-2xl mx-auto bg-white dark:bg-gray-900 py-10">
-    <h1 class="lg:text-6xl text-3xl mt-4 text-black dark:text-white font-extrabold tracking-widest uppercase text-center mb-8">
+  <div class="max-w-screen-2xl bg-yellow-50 mx-auto p-4 my-12 rou dark:bg-gray-900 py-10">
+    <h1 class="lg:text-6xl text-3xl mt-4 text-black dark:text-white rounded-xl font-extrabold tracking-widest uppercase text-center mb-8">
       Upcoming Festivals
     </h1>
 
@@ -8,7 +8,7 @@
       <article
         v-for="ev in events"
         :key="ev.id"
-        class="rounded shadow-lg bg-white dark:bg-gray-800 hover:shadow-md transition-all"
+        class="rounded  bg-white dark:bg-gray-800 hover:shadow-md transition-all"
       >
         <div class="flex items-start justify-between p-4">
           <div class="text-sm">
