ea1c4653069cccffca0c61e0da9337c2dc8c7a36
dhanushkabndara <chathudhanushka77@gmail.com>
2025-08-09 14:50:21 +0530

    ddd

--- resources/js/components/UpComingFestivals.vue
+++ resources/js/components/UpComingFestivals.vue
@@ -4,7 +4,7 @@
       Upcoming Festivals
     </h1>
 
-    <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
+    <div class="grid grid-cols-2 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       <article
         v-for="ev in events"
         :key="ev.id"
