953ba0295ea8127e0f598c68c1a321a617d097fd
dhanushkabndara <chathudhanushka77@gmail.com>
2025-08-18 13:49:49 +0530

    development

---
+++ resources/js/components/admin/ManagePartnerInquiries.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="manage-partner-inquiries">
+    <!-- ManagePartnerInquiries component -->
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ManagePartnerInquiries',
+  components: {},
+  props: {},
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.manage-partner-inquiries { }
+</style>
