e17eb209131789e0ae9d57e8329e618ffc659b4c
kineze <krishantharaka@outlook.com>
2025-08-19 19:26:28 +0530

    update

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -518,31 +518,7 @@
 </section>
 
 
-<section class="relative w-full max-w-screen-2xl mx-auto">
 
-    <div class="w-full bg-white border-black border p-3 flex items-center flex-wrap">
-        <div class="lg:w-2/12 w-full">
-            <div class="aspect-w-1 aspect-h-1  w-full h-56 p-6">
-                <video autoplay muted loop playsinline class="w-full  h-full object-contain rounded-xl">
-                    <source src="{{ asset('/assets/img/dog.webm') }}" type="video/webm">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-        </div>
-        <div class="lg:w-8/12 w-full">
-            <p class="text-lg text-black font-semibold">Our conference is proudly pet-friendly and family-friendly, creating a warm, inclusive space for everyone. Whether you're attending with children or furry companions, you're welcome here. We believe innovation thrives in joyful, supportive environments—so come as you are, bring your whole family, and feel at home.</p>
-        </div>
-        <div class="lg:w-2/12 w-full">
-            <div class="aspect-w-1 aspect-h-1  w-full h-56">
-                <video autoplay muted loop playsinline class="w-full  h-full object-contain rounded-xl">
-                    <source src="{{ asset('/assets/img/cat.webm') }}" type="video/webm">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-        </div>
-    </div>
-
-</section>
 
 
 <section class="w-full max-w-screen-2xl  mt-6 mx-auto">
@@ -582,6 +558,31 @@
    
 </section>
 
+<section class="relative w-full max-w-screen-2xl mx-auto">
+
+    <div class="w-full bg-white border-black border p-3 flex items-center flex-wrap">
+        <div class="lg:w-2/12 w-full">
+            <div class="aspect-w-1 aspect-h-1  w-full h-56 p-6">
+                <video autoplay muted loop playsinline class="w-full  h-full object-contain rounded-xl">
+                    <source src="{{ asset('/assets/img/dog.webm') }}" type="video/webm">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+        <div class="lg:w-8/12 w-full">
+            <p class="text-lg text-black font-semibold">Our conference is proudly pet-friendly and family-friendly, creating a warm, inclusive space for everyone. Whether you're attending with children or furry companions, you're welcome here. We believe innovation thrives in joyful, supportive environments—so come as you are, bring your whole family, and feel at home.</p>
+        </div>
+        <div class="lg:w-2/12 w-full">
+            <div class="aspect-w-1 aspect-h-1  w-full h-56">
+                <video autoplay muted loop playsinline class="w-full  h-full object-contain rounded-xl">
+                    <source src="{{ asset('/assets/img/cat.webm') }}" type="video/webm">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 <section class="w-full max-w-screen-2xl  mt-6 mx-auto">
 
