f146a7d049820f24a76df2d79997687fec166b1b
kineze <krishantharaka@outlook.com>
2025-08-19 23:51:39 +0530

    udate

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -484,7 +484,7 @@
       <img src="{{ asset('assets/img/skyline-shadow.webp') }}" class="w-full object-contain" alt="Layer 2">
     </div>
 
-    <div class="parallax-layer absolute  overflow-hidden z-[120] w-full -top-16 "  data-speed="0.4">
+    <div class="parallax-layer absolute  overflow-hidden z-[120] w-full -top-16 -right-44"  data-speed="0.4">
        <div class="w-full">
         <svg xmlns="http://www.w3.org/2000/svg" stroke="#000000" xmlns:xlink="http://www.w3.org/1999/xlink" width="1080" zoomAndPan="magnify" viewBox="0 0 810 1012.49997" height="1350" preserveAspectRatio="xMidYMid meet" version="1.2">
             <defs>
