4396dfab91c9e29c780f269df680a800b8844047
kineze <krishantharaka@outlook.com>
2025-08-19 19:22:56 +0530

    update

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -303,11 +303,11 @@
 
 
     <div class="relative z-60  flex flex-col items-start justify-center pt-5 p-3">
-      <h2 class="font-semibold text-black uppercase text-8xl">2025</h2>
+      <h2 class="font-semibold text-black uppercase text-8xl">2026</h2>
       <h1 class="tracking-[5px] text-5xl uppercase font-bold text-black leading-tight">
         The Asian Literary <br> Festival
       </h1>
-      {{-- <h2 class="font-semibold tracking-[10px] text-black uppercase text-3xl">Gampaha</h2> --}}
+      <h2 class="font-semibold tracking-[10px] text-black uppercase text-3xl">global summit</h2>
     </div>
 
   </div>
