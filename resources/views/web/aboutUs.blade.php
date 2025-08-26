e06daf43abbdb7008ead48eaae27537009e92400
kineze <krishantharaka@outlook.com>
2025-08-26 22:44:18 +0530

    tes

--- resources/views/web/aboutUs.blade.php
+++ resources/views/web/aboutUs.blade.php
@@ -154,7 +154,7 @@
   ></vision-mission>
 </section>
 
-<section class="w-full max-w-screen-2xl mt-10 mx-auto p-3">
+{{-- <section class="w-full max-w-screen-2xl mt-10 mx-auto p-3">
 
     <div class="bg-white px-4 py-2 mb-5">
       <h1 class="lg:text-6xl text-3xl text-black font-extrabold tracking-widest uppercase text-center">
@@ -163,7 +163,7 @@
     </div>
 
     <read-more-text></read-more-text>
-</section>
+</section> --}}
 
 {{-- <section class="w-full max-w-screen-2xl  mx-auto p-3">
 
@@ -183,11 +183,25 @@
    
 </section>
 
-<section class="w-full max-w-screen-2xl  mx-auto p-3">
+
+
+<section class="w-full max-w-screen-2xl  mt-6 mx-auto">
+
+    <div class=" px-4 py-2 mb-5">
+      <h1 class="lg:text-6xl text-3xl text-black font-extrabold tracking-widest uppercase text-center">
+        Our Ecosystem
+      </h1>
+    </div>
+
+    <our-ecosystem></our-ecosystem>
+   
+</section>
+
+{{-- <section class="w-full max-w-screen-2xl  mx-auto p-3">
 
     <next-count-down></next-count-down>
 
-</section>
+</section> --}}
 
 <section class="w-full max-w-screen-2xl -mt-10 mx-auto p-3">
 
