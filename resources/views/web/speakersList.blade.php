198ef6a47c452158c7a015eac9d5fe4165bd7469
dhanushkabndara <chathudhanushka77@gmail.com>
2025-09-17 15:53:10 +0530

    dev

--- resources/views/web/speakersList.blade.php
+++ resources/views/web/speakersList.blade.php
@@ -4,16 +4,17 @@
 
 <section class="w-full max-w-screen-2xl  -mt-6 mx-auto p-3">
    
-    <div class="w-full rounded-xl bg-cover bg-top z-[200] font-nordique py-9 px-4 lg:pl-10 ">
-      <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
-        <div class="relative z-30 flex flex-col items-start justify-center">
-          <h2 class="font-bold text-primary-blue text-8xl">2026</h2>
-          <h1 class="tracking-[10px] lg:text-5xl text-4xl font-bold pt-4 text-primary-blue">
-            Southern <br>Chronicles
-          </h1>
-          <h2 class="font-semibold tracking-[10px] text-black uppercase text-3xl"></h2>
+    <div class="w-full rounded-xl bg-cover font-nordique bg-top z-[200] py-8 px-4 lg:pl-5">
+        <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
+            <div class="relative z-30 flex flex-col items-start justify-center ">
+                <h2 class="font-bold text-primary-blue uppercase text-8xl">2026</h2>
+                <h1 class="tracking-[8px] lg:text-5xl text-4xl  font-bold pt-6 text-primary-blue leading-tight">
+                    Southern <br>Chronicles
+                </h1>
+                <h2 class="font-semibold tracking-[10px] text-primary-blue uppercase text-3xl"></h2>
+                            {{-- <img src="{{ asset('assets/img/sou-logo.webp')}}" alt="" class="w-48 lg:w-72 object-contain"> --}}
+            </div>
         </div>
-      </div>
     </div>
 
 </section>
--- resources/views/web/viewCareer.blade.php
+++ resources/views/web/viewCareer.blade.php
@@ -5,17 +5,18 @@
 <section class="w-full max-w-screen-2xl  -mt-6 mx-auto p-3">
    
    <div class="w-full rounded-xl bg-cover bg-top">
-      <div class="w-full rounded-xl bg-cover bg-top z-[200] py-9 px-4 lg:pl-10 ">
-            <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
-                <div class="relative z-30 flex flex-col items-start justify-center pt-12">
-                    <h2 class="font-bold text-black uppercase text-8xl">2026</h2>
-                    <h1 class="tracking-[10px] lg:text-5xl text-4xl uppercase font-bold text-black leading-tight">
-                        Southern <br>Chronicles
-                    </h1>
-                    <h2 class="font-semibold tracking-[10px] text-black uppercase text-3xl"></h2>
-                </div>
+      <div class="w-full rounded-xl bg-cover font-nordique bg-top z-[200] py-8 px-4 lg:pl-5">
+        <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
+            <div class="relative z-30 flex flex-col items-start justify-center ">
+                <h2 class="font-bold text-primary-blue uppercase text-8xl">2026</h2>
+                <h1 class="tracking-[8px] lg:text-5xl text-4xl  font-bold pt-6 text-primary-blue leading-tight">
+                    Southern <br>Chronicles
+                </h1>
+                <h2 class="font-semibold tracking-[10px] text-primary-blue uppercase text-3xl"></h2>
+                            {{-- <img src="{{ asset('assets/img/sou-logo.webp')}}" alt="" class="w-48 lg:w-72 object-contain"> --}}
             </div>
         </div>
+    </div>
 
 
         <career-view :career-id="{{ $career->id }}"></career-view>
