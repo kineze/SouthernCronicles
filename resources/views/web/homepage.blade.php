cc07d4caf4fe8344b8f3fb7dd368b0739b25144e
kineze <krishantharaka@outlook.com>
2025-09-19 00:05:42 +0530

    update

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -31,7 +31,7 @@
                 </div>
             
                 <div class="parallax-layer absolute top-16  z-10 w-full" data-speed="0.2">
-                    <img src="{{ asset('assets/img/southern-hro-004.webp') }}" 
+                    <img src="{{ asset('assets/img/souther-hero-005.webp') }}" 
                         class="w-full object-contain" alt="Layer 2">
                 </div>
 
