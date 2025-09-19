fa74540c17c48df9e5fe53cba3eddab0658cf3a3
dhanushkabndara <chathudhanushka77@gmail.com>
2025-09-19 14:24:52 +0530

    deve

--- resources/js/components/web/webNav.vue
+++ resources/js/components/web/webNav.vue
@@ -29,7 +29,7 @@
           <a href="/">
             <img
               :src="logoUrl"
-              class="inline-block max-w-full transition-all  lg:w-44 w-20 duration-200 ease-soft-in-out "
+              class="inline-block max-w-full transition-all  lg:w-36 w-20 duration-200 ease-soft-in-out "
               alt="main_logo"
             />  
           </a>
--- resources/views/web/newsList.blade.php
+++ resources/views/web/newsList.blade.php
@@ -10,9 +10,9 @@
    <div class="w-full rounded-xl bg-cover bg-top">
         <div class="w-full rounded-xl bg-cover font-nordique bg-top z-[200] py-8 px-4 lg:pl-5">
         <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
-            <div class="relative z-30 flex flex-col items-start justify-center ">
+            <div class="relative z-30 flex flex-col items-start justify-center pt-3">
                 <h2 class="font-bold text-primary-blue uppercase text-8xl">2026</h2>
-                <h1 class="tracking-[8px] lg:text-5xl text-4xl  font-bold pt-6 text-primary-blue leading-tight">
+                <h1 class="tracking-[8px] lg:text-5xl text-4xl  font-bold pt-3 text-primary-blue leading-tight">
                     Southern <br>Chronicles
                 </h1>
                 <h2 class="font-semibold tracking-[10px] text-primary-blue uppercase text-3xl"></h2>
