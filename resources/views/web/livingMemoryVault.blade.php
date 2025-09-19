88ed9ee1a428776e77cf7e3dfea70e8ad43f338c
dhanushkabndara <chathudhanushka77@gmail.com>
2025-09-19 14:47:19 +0530

    dev

--- resources/views/web/livingMemoryVault.blade.php
+++ resources/views/web/livingMemoryVault.blade.php
@@ -7,7 +7,7 @@
     <div class="w-full rounded-xl bg-cover bg-top">
         <div class="w-full rounded-xl bg-cover font-nordique bg-top z-[200] py-8 px-4 lg:pl-5">
         <div class='w-full rounded-xl rounded-bl-none bg-transparent flex bg-cover items-start'>
-            <div class="relative z-30 flex flex-col items-start justify-center pt-3">
+            <div class="relative z-30 flex flex-col items-start justify-center pt-12">
                 <h2 class="font-bold text-primary-blue uppercase text-8xl">2026</h2>
                 <h1 class="tracking-[8px] lg:text-5xl text-4xl  font-bold pt-3 text-primary-blue leading-tight">
                     Southern <br>Chronicles
