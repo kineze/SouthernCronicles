a2ea1924886ffeb68b470f7fcb9216c7188ebc90
kineze <krishantharaka@outlook.com>
2026-04-09 10:02:08 +0530

    upadte

--- resources/views/web/includes/headerlinks.blade.php
+++ resources/views/web/includes/headerlinks.blade.php
@@ -8,7 +8,7 @@
 
     <meta property="og:title" content="The Asian Literary Festival - jaffna" />
     <meta property="og:description" content="Bridging Worlds Through Words" />
-    <meta property="og:image" content="{{ asset('assets/img/og-image.webp') }}" />
+    <meta property="og:image" content="{{ asset('assets/img/og-image-new.webp') }}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url()->current() }}" />
     
