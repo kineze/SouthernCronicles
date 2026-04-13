118e160b78c082df4e05a17c75e6f4ff21377d87
kineze <krishantharaka@outlook.com>
2026-04-13 18:39:11 +0530

    update

--- resources/views/web/includes/headerlinks.blade.php
+++ resources/views/web/includes/headerlinks.blade.php
@@ -1,16 +1,21 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="gmaps-key" content="{{ config('services.google_maps.key') }}">
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/logo.webp')}}" />
     <link rel="icon" type="image/png" href="{{asset('assets/img/logo.webp')}}">
 
 
     <meta property="og:title" content="The Asian Literary Festival - jaffna" />
     <meta property="og:description" content="Bridging Worlds Through Words" />
-    <meta property="og:image" content="{{ asset('assets/img/vaanam-og.webp') }}" />
+    <meta property="og:image" content="{{ asset('assets/img/vaanam-og.jpg', true) }}" />
+    <meta property="og:image:secure_url" content="{{ asset('assets/img/vaanam-og.jpg', true) }}" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="800" />
+    <meta property="og:image:height" content="451" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url()->current() }}" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image" content="{{ asset('assets/img/vaanam-og.jpg', true) }}" />
     
     <title>The Asian Literary Festival - jaffna</title>
 
